1114501160f20b4cd880bad33bda4d1875687337
Minchan Jung <minchanjung1@gmail.com>
2022-05-29 14:05:54 +0100

    created router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import RouterSwitch from './RouteSwitch';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterSwitch />
   </React.StrictMode>
 );
 
